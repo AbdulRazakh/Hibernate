2d7b5899df3fdb493b1c766c25211c33e8021de9
AbdulRazakh <abdulrazakh.brb@gmail.com>
2018-04-09 12:10:21 +0530

    simple add queary

---
+++ hibernateInsertQueary/src/main/java/com/abdul/main/ClientForSave.java
@@ -0,0 +1,34 @@
+package com.abdul.main;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class ClientForSave {
+
+	public static void main(String[] args) {
+		Configuration configuration = new Configuration();
+	    configuration.configure("hibernate.cfg.xml");
+	    ServiceRegistry serviceRegistry =  new StandardServiceRegistryBuilder().applySettings(
+	            configuration.getProperties()).build();
+	    SessionFactory sessionFactory=configuration.buildSessionFactory(serviceRegistry);
+		Session session = sessionFactory.openSession();
+		Product product=new Product();
+		product.setProductId(121);
+		product.setProductName("arproduct");
+		product.setProductPrice(20000);
+		Transaction transaction=session.beginTransaction();
+		session.save(product);
+		System.out.println("Obeject saved successfulyy");
+		transaction.commit();
+		session.close();
+		sessionFactory.close();
+		System.out.println("session closed successfulyy.........");
+		
+
+	}
+
+}

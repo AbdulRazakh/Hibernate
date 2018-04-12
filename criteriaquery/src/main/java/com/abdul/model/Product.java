43c5c6404e8fe20bdf38171bc4dc7620e363b377
AbdulRazakh <abdulrazakh.brb@gmail.com>
2018-04-12 08:09:09 +0530

    added criteria

---
+++ criteriaquery/src/main/java/com/abdul/model/Product.java
@@ -0,0 +1,28 @@
+package com.abdul.model;
+
+public class Product {
+	
+	private int productid;
+	private String productName;
+	private double productPrice;
+	public int getProductid() {
+		return productid;
+	}
+	public void setProductid(int productid) {
+		this.productid = productid;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public double getProductPrice() {
+		return productPrice;
+	}
+	public void setProductPrice(double productPrice) {
+		this.productPrice = productPrice;
+	}
+	
+
+}

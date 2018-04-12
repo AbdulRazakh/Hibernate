bb52709477234e4f09913a702567869dc68620fb
AbdulRazakh <abdulrazakh.brb@gmail.com>
2018-04-12 08:27:32 +0530

    order in ascending order

---
+++ ordercriteria/src/main/java/com/abdul/java/ClientJava.java
@@ -0,0 +1,50 @@
+package com.abdul.java;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.service.ServiceRegistry;
+
+import com.abdul.model.Product;
+
+public class ClientJava {
+
+	public static void main(String[] args) {
+		
+		Configuration configuration = new Configuration();
+	    configuration.configure("hibernate.cfg.xml");
+	    ServiceRegistry serviceRegistry =  new StandardServiceRegistryBuilder().applySettings(
+	            configuration.getProperties()).build();
+	    SessionFactory sessionFactory=configuration.buildSessionFactory(serviceRegistry);
+		Session session = sessionFactory.openSession();
+		
+		
+		Criteria criteria=session.createCriteria(Product.class);
+		Criterion con=Restrictions.gt("productPrice", new Double(1000));
+		criteria.add(con);
+		criteria.addOrder(Order.asc("productPrice"));
+		List list=criteria.list();
+		Iterator itr=list.iterator();
+		while (itr.hasNext()) {
+			Product object = (Product) itr.next();
+			System.out.println("product id"+object.getProductId());
+			System.out.println("product Name"+object.getProductName());
+			System.out.println("product Price"+object.getProductPrice());
+			
+		}
+		session.close();
+		sessionFactory.close();
+		
+	}
+
+	}
+
+
